6bbce3e0ca64c09f2268634eeb0f0f8e23f16c69
allyboy08 <alexsassman65@gmail.com>
2021-02-26 16:16:53 +0200

    update

--- assets/script/login.js
+++ assets/script/login.js
@@ -16,3 +16,51 @@ span.onclick = function () {
 span1.onclick = function () {
   modal1.style.display = "none";
 };
+
+function createAccount() {
+  const inputs = document.getElementsByTagName("input");
+
+  fetch("http://127.0.0.1:5000/add-new/", {
+    method: "POST",
+    body: JSON.stringify({
+      fname: inputs[0].value,
+      uname: inputs[1].value,
+      passw: inputs[2].value,
+      email: inputs[3].value,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      alert("account has been created");
+      console.log(json);
+      document.getElementById("create-account").reset();
+    });
+}
+
+function login() {
+  let loginForm = document.getElementById("login");
+  let inputs = loginForm.getElementsByTagName("input");
+
+  let uname = inputs[0].value;
+  let passw = inputs[1].value;
+
+  let users;
+  fetch("http://127.0.0.1:5000/show-accounts/")
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      users = json;
+      console.log(uname, passw, users);
+
+      let loggedIn = users.filter((user) => {
+        return user.uname == uname && user.passw == passw;
+      });
+
+      if (loggedIn.length >= 1) {
+        window.location.href = "./profile.html";
+      }
+    });
+}
