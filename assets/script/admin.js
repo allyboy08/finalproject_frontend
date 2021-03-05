bb122ce4409cef20f140e875867c7af8fb821dd6
allyboy08 <alexsassman65@gmail.com>
2021-03-05 16:14:42 +0200

    update friday

--- assets/script/admin.js
+++ assets/script/admin.js
@@ -26,7 +26,7 @@ function admin() {
 }
 
 function getAcc() {
-  fetch("http://127.0.0.1:5000/show-accounts/")
+  fetch(`http://127.0.0.1:5000/show-accounts/`)
     .then((response) => response.json())
     .then((json) => {
       console.log(json);
@@ -36,13 +36,14 @@ function getAcc() {
 
 function showtr(user) {
   const tableacc = `<tr data-id=${user.id}>
+  
         <td>${user.id}</td>
-        <td><input name="fname" type='text' value="${user.fname}"></td>
-        <td><input name="uname" type='text' value="${user.uname}"></td>
-        <td><input name="passw" type='text' value="${user.passw}"></td>
-        <td><input name="email" type='text' value="${user.email}"></td>
-        <td><button onclick="deleteAcc(${user.id})">delete</button></td>
-        <td><button onclick="editAcc(${user.id})">edit</button></td>
+        <td><input class="inv" name="fname" type='text' value="${user.fname}"></td>
+        <td><input class="inv" name="uname" type='text' value="${user.uname}"></td>
+        <td><input class="inv" name="passw" type='text' value="${user.passw}"></td>
+        <td><input class="inv" name="email" type='text' value="${user.email}"></td>
+        <td><button class="tdbtn" onclick="deleteAcc(${user.id})">delete</button></td>
+        <td><button class="tebtn" onclick="editAcc(${user.id})">edit</button></td>
       </tr>`;
   let list = document.getElementById("acc-table");
   console.log("hello");
@@ -58,4 +59,27 @@ function deleteAcc(id) {
     alert("cancel");
     console.log("not saved to database");
   }
+}
+
+function editAcc(id) {
+  let userItem = document.querySelector(`[data-id="${id}"]`);
+  let inputs = userItem.getElementsByTagName("input");
+  console.log(userItem);
+
+  let user = {
+    fname: inputs[0].value,
+    uname: inputs[1].value,
+    passw: inputs[2].value,
+    email: inputs[3].value,
+  };
+
+  fetch(`http://127.0.0.1:5000/edit-account/${id}/`, {
+    method: "PUT",
+    body: JSON.stringify(user),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
 }
